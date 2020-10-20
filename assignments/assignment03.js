44f71f405bbfabbf61f7653ab7a61e71a3dac561
haxhaxjames <70585560+haxhaxjames@users.noreply.github.com>
2020-10-19 21:32:13 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -1,7 +1,6 @@
 let elem = [];
 // assign the entire table row for hole 1 to a variable, elem
-elem[1]
-  = document.getElementById("1");
+elem[1] = document.getElementById("1");
 
 // display the number of children (all td elements)
 // console.log(elem.children.length);
@@ -9,8 +8,7 @@ elem[1]
 // console.log(elem.children[4].children[0]); 
 
 // assign a function to the + button
-elem[1].children[4].children[0].onclick 
-  = function(){add1(elem[1]);};
+elem[1].children[4].children[0].onclick = function(){add1(elem[1]);};
 
 // create an "add1" function
 function add1 (elem) {
